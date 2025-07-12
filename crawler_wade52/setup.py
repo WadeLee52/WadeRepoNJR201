7da571e8cc69d824efb3a3f3c91c6848cd47f845
wade <w24810@gmail.com>
2025-07-12 09:27:41 +0800

    修改setup.py

--- crawler_wade52/setup.py
+++ crawler_wade52/setup.py
@@ -9,13 +9,13 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 
 setup(
-    name="crawler",  # Required
+    name="crawler_wade52",  # Required
     version="0.0.1",  # Required
     description="test",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
-    author="linsam",  # Optional
-    author_email="samlin266118@gmail.com",  # Optional
+    author="wade",  # Optional
+    author_email="w24810@gmail.com",  # Optional
     classifiers=[  # Optional
         "Development Status :: 3 - Alpha",
     ],
