dc45126676924971a28f1fb49c2c1ba5ad9eb3c0
wade <w24810@gmail.com>
2025-07-20 05:43:42 +0800

    修改producer路徑

--- dataflow_wade52/src/dataflow/etl/producer_twse.py
+++ dataflow_wade52/src/dataflow/etl/producer_twse.py
@@ -10,7 +10,7 @@ def create_producer_twse_task() -> DockerOperator:
         # 設定這個 task 在 DAG 中的名稱（唯一識別碼）
         task_id="producer_crawler_finmind_duplicate",
         image="wade52/tibame_crawler:0.0.6",
-        command="pipenv run python crawler/producer_crawler_finmind_duplicate.py",
+        command="pipenv run python crawlerwade52/producer_crawler_finmind_duplicate.py",
         # 每次執行前都強制重新拉取最新的 image（確保使用最新版本）
         force_pull=True,
         # 容器執行完畢後自動刪除（避免堆積殘留容器）
